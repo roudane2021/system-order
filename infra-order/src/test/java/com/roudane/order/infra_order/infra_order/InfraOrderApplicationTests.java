f66a445a63da383d1ba2487583c7672918da03d1
roudane2021 <ra.roudane@gmail.com>
2025-01-20 22:19:42 +0100

    first commit

---
+++ infra-order/src/test/java/com/roudane/order/infra_order/infra_order/InfraOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roudane.order.infra_order.infra_order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraOrderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
