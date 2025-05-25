9d8f06e59a294686e353005298a6e19b75f9d51b
rachid.roudane <ra.roudane@gmail.com>
2025-05-25 15:13:36 +0200

    ajouter la classe config pour le module Order

---
+++ infra-notification/src/main/java/com/roudane/order/infra_notification/InfraNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.roudane.order.infra_notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraNotificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfraNotificationApplication.class, args);
+	}
+
+}
