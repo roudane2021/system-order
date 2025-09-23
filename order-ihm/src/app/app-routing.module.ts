51cb28949c6ae5726079358f9aefdb4de718374e
roudane2021 <ra.roudane@gmail.com>
2025-09-23 22:48:55 +0200

    feat:020925: ajout de spring cloud

--- order-ihm/src/app/app-routing.module.ts
+++ order-ihm/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'order', loadChildren: () => import('./order/order.module').then(m => m.OrderModule) },
+  { path: '**', redirectTo: 'order'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
